3fb1389bb8510472efaac940bf119a6dcab2fbab
MiroshnikNatalie <mn9223632@gmail.com>
2020-12-03 21:11:58 +0200

    дз 02999

---
+++ src/lesson3.java
@@ -0,0 +1,15 @@
+package com.academy.telesens;
+
+public class lesson3 {
+    public static void main(String[] args) {
+
+        for ( int i = 1; i< 100; i+=2)
+
+        {
+            System.out.println(i);
+        }
+
+
+    }
+
+}
